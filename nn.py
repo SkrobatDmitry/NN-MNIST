44f74114b064bdb235a3a2c9d3354c5c58f4a37f
Dima <skrobat.dima@gmail.com>
2021-11-11 18:15:08 +0300

    Added forward propogation.
         (ReLU)    (Softmax)
    input  ->  hidden  ->  output

--- nn.py
+++ nn.py
@@ -1,16 +1,29 @@
 import numpy as np
+from functions import ReLU, Softmax
 
 
 class NeuralNetwork:
     def __init__(self):
         self.parameters = self.init_parameters(784, 128, 11)
         self.velocity = self.init_velocity()
+        self.cache = {}
 
     def forward(self, x):
-        pass
+        z1 = self.linear_function(self.parameters['w1'], x, self.parameters['b1'])
+        h = ReLU.activation(z1)
+
+        z2 = self.linear_function(self.parameters['w2'], h, self.parameters['b2'])
+        o = Softmax.activation(z2)
+
+        self.cache = {'z1': z1, 'h': h, 'z2': z2, 'o': o}
+        return o
 
     def backward(self, x, error):
         pass
+
+    @staticmethod
+    def linear_function(w, x, b):
+        return np.dot(w, x) + b
 
     def init_parameters(self, n_in, n_h, n_out):
         return {'w1': self.init_weights(n_in, n_h), 'b1': self.init_biases(n_h),
